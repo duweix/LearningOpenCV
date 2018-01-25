cc77962b8c922bda084ec51bb1989825492f619a
duweix <duweix@126.com>
2018-01-25 15:52:01 +0800

    duweix@126.com

--- 例2-3_视频播放控制/index.cpp
+++ 例2-3_视频播放控制/index.cpp
@@ -1,5 +1,7 @@
 #include <opencv2/opencv.hpp>
 
+#pragma comment(lib, "opencv_imgproc231d")
+
 int         g_slider_position   = 0;
 CvCapture   *g_capture          = NULL;
 
@@ -7,8 +9,20 @@ void onTrackbarSlide(int pos) {
     cvSetCaptureProperty(g_capture, CV_CAP_PROP_POS_FRAMES, pos);
 }
 
+IplImage* doCanny(IplImage *in) {
+    IplImage *gray = cvCreateImage(cvGetSize(in), IPL_DEPTH_8U, 1);
+    cvCvtColor(in, gray, CV_BGR2GRAY);
+
+    IplImage *out = cvCreateImage(cvGetSize(in), IPL_DEPTH_8U, 1);
+    cvCanny(gray, out, 10, 200, 3);
+
+    cvReleaseImage(&gray);
+    return out;
+}
+
 int main(int argc, char **argv) {
     cvNamedWindow("Example3", CV_WINDOW_AUTOSIZE);
+    cvNamedWindow("Example3_canny", CV_WINDOW_AUTOSIZE);
     g_capture = cvCreateFileCapture("C:\\Users\\user001\\Pictures\\Saved Pictures\\金字塔造假之谜大揭密_标清.avi");
     int frames = (int) cvGetCaptureProperty(g_capture, CV_CAP_PROP_FRAME_COUNT);
     if (frames != 0) {
@@ -19,15 +33,20 @@ int main(int argc, char **argv) {
             frames,
             onTrackbarSlide);
     }
-    IplImage *frame;
+    IplImage *frame = NULL;
+    IplImage *cannyImg = NULL;
     while (1) {
         frame = cvQueryFrame(g_capture);
         if (!frame) break;
+        cannyImg = doCanny(frame);
         cvShowImage("Example3", frame);
+        cvShowImage("Example3_canny", cannyImg);
+        cvReleaseImage(&cannyImg);
         char c = cvWaitKey(41);
         if (c == 27) break;
     }
     cvReleaseCapture(&g_capture);
     cvDestroyWindow("Example3");
+    cvDestroyWindow("Example3_canny");
     return 0;
 }
--- 例2-6_Canny边缘检测将输出写入一个单通道（灰度级）图像/index.cpp
+++ 例2-6_Canny边缘检测将输出写入一个单通道（灰度级）图像/index.cpp
@@ -1,6 +1,27 @@
+#ifndef _PREDEP_H_
+#define _PREDEP_H_
+
+#pragma once
+
 #include <opencv2/opencv.hpp>
 
+#define CV_VERSION_ID \
+CVAUX_STR(CV_MAJOR_VERSION) \
+CVAUX_STR(CV_MINOR_VERSION) \
+CVAUX_STR(CV_SUBMINOR_VERSION) \
+
+#ifdef _DEBUG
+#define cvLIB(name) "opencv_" name CV_VERSION_ID "d"
+#else
+#define cvLIB(name) "opencv_" name CV_VERSION_ID
+#endif
+
 #pragma comment(linker, "/subsystem:windows /entry:mainCRTStartup")
+#pragma comment(lib, cvLIB("core"))
+#pragma comment(lib, cvLIB("highgui"))
+#pragma comment(lib, cvLIB("imgproc"))
+
+#endif
 
 IplImage* doCanny(IplImage  *in,
                   double    lowThresh,
